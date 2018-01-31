8f87c78bef41a5ff6d59e69b248447646443a238
KampusHub <verbelesteban2@gmail.com>
2018-01-30 20:31:16 -0500

    Created Mobile App Project

---
+++ ImageTextTranslatorApp/ImageTextTranslatorApp/ImageTextTranslatorApp.Android/Fragments/IFragmentVisible.cs
@@ -0,0 +1,7 @@
+﻿namespace ImageTextTranslatorApp.Droid
+{
+	interface IFragmentVisible
+	{
+		void BecameVisible();
+	}
+}
---
+++ ImageTextTranslatorApp/ImageTextTranslatorApp/ImageTextTranslatorApp/App.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ImageTextTranslatorApp.Helpers;
+using ImageTextTranslatorApp.Models;
+using ImageTextTranslatorApp.Services;
+
+namespace ImageTextTranslatorApp
+{
+    public class App
+    {
+
+        public static void Initialize()
+        {
+            ServiceLocator.Instance.Register<IDataStore<Item>, MockDataStore>();
+        }
+    }
+}
