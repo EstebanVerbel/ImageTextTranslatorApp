19e8d796bd668e3c4e6094885568b573d2057a99
KampusHub <verbelesteban2@gmail.com>
2018-02-02 21:52:04 -0500

    Added PictureViewModel in Shared code

---
+++ ImageTextTranslatorApp/ImageTextTranslatorApp/ImageTextTranslatorApp/ViewModels/PictureViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Input;
+
+namespace ImageTextTranslatorApp.ViewModels
+{
+    public class PictureViewModel : BaseViewModel
+    {
+
+        public PictureViewModel()
+        {
+            Title = "Picture";
+
+            // TODO: Set implementation of TakePictureCommand (Get from Android/iOS Projects)
+            // maybe pass through constructor
+        }
+
+        public ICommand TakePictureCommand { get; }
+
+    }
+}
