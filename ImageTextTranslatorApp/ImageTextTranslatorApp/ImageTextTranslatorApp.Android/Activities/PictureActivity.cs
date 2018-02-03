e2a5baa2fc9653e5ad88a353490112bf9fb396e1
KampusHub <verbelesteban2@gmail.com>
2018-02-02 21:46:47 -0500

    Added the PictureActivity

---
+++ ImageTextTranslatorApp/ImageTextTranslatorApp/ImageTextTranslatorApp.Android/Activities/PictureActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ImageTextTranslatorApp.Droid.Activities
+{
+    
+    [Activity(Label = "Take Picture", ParentActivity = typeof(MainActivity))]
+    [MetaData("android.support.PARENT_ACTIVITY", Value = ".MainActivity")]
+    public class PictureActivity : Activity
+    {
+        // TODO: Specify the layout to inflace
+        
+        // PictureViewModel viewModel;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            // TODO: Instanciate ViewModel
+        }
+        
+        protected override void OnStart()
+        {
+            base.OnStart();
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+        }
+        
+    }
+}
