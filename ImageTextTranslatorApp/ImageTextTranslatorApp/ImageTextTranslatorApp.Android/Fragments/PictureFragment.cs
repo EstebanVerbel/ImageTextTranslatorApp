a52bab1cd5f45c4e8c1d1b8934caf32b527fc0bc
KampusHub <verbelesteban2@gmail.com>
2018-02-08 21:31:47 -0500

    Added picture fragment layout with button and image view

--- ImageTextTranslatorApp/ImageTextTranslatorApp/ImageTextTranslatorApp.Android/Fragments/PictureFragment.cs
+++ ImageTextTranslatorApp/ImageTextTranslatorApp/ImageTextTranslatorApp.Android/Fragments/PictureFragment.cs
@@ -1,11 +1,13 @@
 ﻿using Android.OS;
 using Android.Views;
+using Android.Widget;
 using System;
 
 namespace ImageTextTranslatorApp.Droid
 {
     public class PictureFragment : Android.Support.V4.App.Fragment, IFragmentVisible
     {
+        private Button _takePictureButton;
         
         public override void OnCreate(Bundle savedInstanceState)
         {
@@ -16,11 +18,15 @@ namespace ImageTextTranslatorApp.Droid
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            // Use this to return your custom view for this Fragment
-            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+            // Inflate Picture Fragment
+            var view = inflater.Inflate(Resource.Layout.fragment_picture, container, false);
 
-            return base.OnCreateView(inflater, container, savedInstanceState);
+            _takePictureButton = view.FindViewById<Button>(Resource.Id.takePictureButton);
+
+            return view;
         }
+
+        
 
 
         public void BecameVisible()
--- ImageTextTranslatorApp/ImageTextTranslatorApp/ImageTextTranslatorApp.Android/Resources/Resource.designer.cs
+++ ImageTextTranslatorApp/ImageTextTranslatorApp/ImageTextTranslatorApp.Android/Resources/Resource.designer.cs
@@ -1940,8 +1940,8 @@ namespace ImageTextTranslatorApp.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060085
-			public const int action0 = 2131099781;
+			// aapt resource value: 0x7f060087
+			public const int action0 = 2131099783;
 			
 			// aapt resource value: 0x7f06005a
 			public const int action_bar = 2131099738;
@@ -1967,8 +1967,8 @@ namespace ImageTextTranslatorApp.Droid
 			// aapt resource value: 0x7f06005b
 			public const int action_context_bar = 2131099739;
 			
-			// aapt resource value: 0x7f060089
-			public const int action_divider = 2131099785;
+			// aapt resource value: 0x7f06008b
+			public const int action_divider = 2131099787;
 			
 			// aapt resource value: 0x7f060003
 			public const int action_menu_divider = 2131099651;
@@ -2015,8 +2015,8 @@ namespace ImageTextTranslatorApp.Droid
 			// aapt resource value: 0x7f060072
 			public const int button_signin = 2131099762;
 			
-			// aapt resource value: 0x7f060086
-			public const int cancel_action = 2131099782;
+			// aapt resource value: 0x7f060088
+			public const int cancel_action = 2131099784;
 			
 			// aapt resource value: 0x7f06002b
 			public const int center = 2131099691;
@@ -2030,8 +2030,8 @@ namespace ImageTextTranslatorApp.Droid
 			// aapt resource value: 0x7f060052
 			public const int checkbox = 2131099730;
 			
-			// aapt resource value: 0x7f06008c
-			public const int chronometer = 2131099788;
+			// aapt resource value: 0x7f06008e
+			public const int chronometer = 2131099790;
 			
 			// aapt resource value: 0x7f060033
 			public const int clip_horizontal = 2131099699;
@@ -2090,8 +2090,8 @@ namespace ImageTextTranslatorApp.Droid
 			// aapt resource value: 0x7f06001c
 			public const int end = 2131099676;
 			
-			// aapt resource value: 0x7f060091
-			public const int end_padder = 2131099793;
+			// aapt resource value: 0x7f060093
+			public const int end_padder = 2131099795;
 			
 			// aapt resource value: 0x7f060023
 			public const int enterAlways = 2131099683;
@@ -2135,8 +2135,8 @@ namespace ImageTextTranslatorApp.Droid
 			// aapt resource value: 0x7f06003f
 			public const int image = 2131099711;
 			
-			// aapt resource value: 0x7f060090
-			public const int info = 2131099792;
+			// aapt resource value: 0x7f060092
+			public const int info = 2131099794;
 			
 			// aapt resource value: 0x7f060000
 			public const int item_touch_helper_previous_elevation = 2131099648;
@@ -2144,11 +2144,11 @@ namespace ImageTextTranslatorApp.Droid
 			// aapt resource value: 0x7f06002f
 			public const int left = 2131099695;
 			
-			// aapt resource value: 0x7f06008a
-			public const int line1 = 2131099786;
+			// aapt resource value: 0x7f06008c
+			public const int line1 = 2131099788;
 			
-			// aapt resource value: 0x7f06008e
-			public const int line3 = 2131099790;
+			// aapt resource value: 0x7f060090
+			public const int line3 = 2131099792;
 			
 			// aapt resource value: 0x7f06000b
 			public const int listMode = 2131099659;
@@ -2159,11 +2159,11 @@ namespace ImageTextTranslatorApp.Droid
 			// aapt resource value: 0x7f060069
 			public const int main_content = 2131099753;
 			
-			// aapt resource value: 0x7f060088
-			public const int media_actions = 2131099784;
+			// aapt resource value: 0x7f06008a
+			public const int media_actions = 2131099786;
 			
-			// aapt resource value: 0x7f060093
-			public const int menu_edit = 2131099795;
+			// aapt resource value: 0x7f060095
+			public const int menu_edit = 2131099797;
 			
 			// aapt resource value: 0x7f06001d
 			public const int middle = 2131099677;
@@ -2191,6 +2191,9 @@ namespace ImageTextTranslatorApp.Droid
 			
 			// aapt resource value: 0x7f060046
 			public const int parentPanel = 2131099718;
+			
+			// aapt resource value: 0x7f060086
+			public const int pictureImageView = 2131099782;
 			
 			// aapt resource value: 0x7f060029
 			public const int pin = 2131099689;
@@ -2285,8 +2288,8 @@ namespace ImageTextTranslatorApp.Droid
 			// aapt resource value: 0x7f060013
 			public const int showTitle = 2131099667;
 			
-			// aapt resource value: 0x7f060092
-			public const int sliding_tabs = 2131099794;
+			// aapt resource value: 0x7f060094
+			public const int sliding_tabs = 2131099796;
 			
 			// aapt resource value: 0x7f06007a
 			public const int snackbar_action = 2131099770;
@@ -2315,8 +2318,8 @@ namespace ImageTextTranslatorApp.Droid
 			// aapt resource value: 0x7f060031
 			public const int start = 2131099697;
 			
-			// aapt resource value: 0x7f060087
-			public const int status_bar_latest_event_content = 2131099783;
+			// aapt resource value: 0x7f060089
+			public const int status_bar_latest_event_content = 2131099785;
 			
 			// aapt resource value: 0x7f060065
 			public const int submit_area = 2131099749;
@@ -2327,11 +2330,14 @@ namespace ImageTextTranslatorApp.Droid
 			// aapt resource value: 0x7f060075
 			public const int tabs = 2131099765;
 			
-			// aapt resource value: 0x7f06008f
-			public const int text = 2131099791;
+			// aapt resource value: 0x7f060085
+			public const int takePictureButton = 2131099781;
 			
-			// aapt resource value: 0x7f06008d
-			public const int text2 = 2131099789;
+			// aapt resource value: 0x7f060091
+			public const int text = 2131099793;
+			
+			// aapt resource value: 0x7f06008f
+			public const int text2 = 2131099791;
 			
 			// aapt resource value: 0x7f06004d
 			public const int textSpacerNoButtons = 2131099725;
@@ -2339,8 +2345,8 @@ namespace ImageTextTranslatorApp.Droid
 			// aapt resource value: 0x7f060080
 			public const int textView1 = 2131099776;
 			
-			// aapt resource value: 0x7f06008b
-			public const int time = 2131099787;
+			// aapt resource value: 0x7f06008d
+			public const int time = 2131099789;
 			
 			// aapt resource value: 0x7f060043
 			public const int title = 2131099715;
@@ -2561,49 +2567,52 @@ namespace ImageTextTranslatorApp.Droid
 			public const int fragment_browse = 2130903082;
 			
 			// aapt resource value: 0x7f03002b
-			public const int item_browse = 2130903083;
+			public const int fragment_picture = 2130903083;
 			
 			// aapt resource value: 0x7f03002c
-			public const int notification_media_action = 2130903084;
+			public const int item_browse = 2130903084;
 			
 			// aapt resource value: 0x7f03002d
-			public const int notification_media_cancel_action = 2130903085;
+			public const int notification_media_action = 2130903085;
 			
 			// aapt resource value: 0x7f03002e
-			public const int notification_template_big_media = 2130903086;
+			public const int notification_media_cancel_action = 2130903086;
 			
 			// aapt resource value: 0x7f03002f
-			public const int notification_template_big_media_narrow = 2130903087;
+			public const int notification_template_big_media = 2130903087;
 			
 			// aapt resource value: 0x7f030030
-			public const int notification_template_lines = 2130903088;
+			public const int notification_template_big_media_narrow = 2130903088;
 			
 			// aapt resource value: 0x7f030031
-			public const int notification_template_media = 2130903089;
+			public const int notification_template_lines = 2130903089;
 			
 			// aapt resource value: 0x7f030032
-			public const int notification_template_part_chronometer = 2130903090;
+			public const int notification_template_media = 2130903090;
 			
 			// aapt resource value: 0x7f030033
-			public const int notification_template_part_time = 2130903091;
+			public const int notification_template_part_chronometer = 2130903091;
 			
 			// aapt resource value: 0x7f030034
-			public const int select_dialog_item_material = 2130903092;
+			public const int notification_template_part_time = 2130903092;
 			
 			// aapt resource value: 0x7f030035
-			public const int select_dialog_multichoice_material = 2130903093;
+			public const int select_dialog_item_material = 2130903093;
 			
 			// aapt resource value: 0x7f030036
-			public const int select_dialog_singlechoice_material = 2130903094;
+			public const int select_dialog_multichoice_material = 2130903094;
 			
 			// aapt resource value: 0x7f030037
-			public const int support_simple_spinner_dropdown_item = 2130903095;
+			public const int select_dialog_singlechoice_material = 2130903095;
 			
 			// aapt resource value: 0x7f030038
-			public const int tabs = 2130903096;
+			public const int support_simple_spinner_dropdown_item = 2130903096;
 			
 			// aapt resource value: 0x7f030039
-			public const int toolbar = 2130903097;
+			public const int tabs = 2130903097;
+			
+			// aapt resource value: 0x7f03003a
+			public const int toolbar = 2130903098;
 			
 			static Layout()
 			{
@@ -2750,6 +2759,9 @@ namespace ImageTextTranslatorApp.Droid
 			
 			// aapt resource value: 0x7f09001a
 			public const int tab2 = 2131296282;
+			
+			// aapt resource value: 0x7f09002a
+			public const int take_picture = 2131296298;
 			
 			// aapt resource value: 0x7f090023
 			public const int text = 2131296291;
