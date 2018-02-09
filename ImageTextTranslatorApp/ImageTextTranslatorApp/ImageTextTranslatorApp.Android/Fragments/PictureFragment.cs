f1580893266ed1a31fc9f4bfe267d96fe57611c0
KampusHub <verbelesteban2@gmail.com>
2018-02-08 21:15:55 -0500

    Added new Picture Fragment

---
+++ ImageTextTranslatorApp/ImageTextTranslatorApp/ImageTextTranslatorApp.Android/Fragments/PictureFragment.cs
@@ -0,0 +1,31 @@
+﻿using Android.OS;
+using Android.Views;
+using System;
+
+namespace ImageTextTranslatorApp.Droid
+{
+    public class PictureFragment : Android.Support.V4.App.Fragment, IFragmentVisible
+    {
+        
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+
+            return base.OnCreateView(inflater, container, savedInstanceState);
+        }
+
+
+        public void BecameVisible()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
