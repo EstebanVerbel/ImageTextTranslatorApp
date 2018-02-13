807602d731e74cf65b63bea59e9cfee78ff71c0f
KampusHub <verbelesteban2@gmail.com>
2018-02-12 21:04:56 -0500

    Added Command to manage service to get image from text

---
+++ ImageTextTranslatorApp/ImageTextTranslatorApp/ImageTextTranslatorApp/Models/Commands/GetImageTextCommand.cs
@@ -0,0 +1,35 @@
+﻿using ImageTextTranslatorApp.Services;
+using System;
+using System.Windows.Input;
+
+namespace ImageTextTranslatorApp.Models.Commands
+{
+    public class GetImageTextCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Picture _picture;
+
+        public GetImageTextCommand(Picture picture)
+        {
+            _picture = picture;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            // TODO: Only if device is connected to internet
+            return true;
+        }
+
+        /// <summary>
+        /// Excecutes the Computer Vision Service to get text from Image
+        /// </summary>
+        /// <param name="parameter">Picture</param>
+        public async void Execute(object parameter)
+        {
+            ComputerVisionService computerVisionService = new Services.ComputerVisionService(_picture.PictureData);
+            string text = await computerVisionService.GetImageTextAsync();
+            _picture.Text = text;
+        }
+    }
+}
