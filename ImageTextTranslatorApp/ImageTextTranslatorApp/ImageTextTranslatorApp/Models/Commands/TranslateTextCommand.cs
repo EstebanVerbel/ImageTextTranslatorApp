0194dde668734cbcb1be4a083a90232379310e50
KampusHub <verbelesteban2@gmail.com>
2018-02-26 21:36:42 -0500

    Added Translate Command to make call to translator service

---
+++ ImageTextTranslatorApp/ImageTextTranslatorApp/ImageTextTranslatorApp/Models/Commands/TranslateTextCommand.cs
@@ -0,0 +1,33 @@
+﻿using ImageTextTranslatorApp.Services;
+using System;
+using System.Windows.Input;
+
+namespace ImageTextTranslatorApp.Models.Commands
+{
+    public class TranslateTextCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Picture _picture;
+
+        public TranslateTextCommand(Picture picture)
+        {
+            _picture = picture;
+            // TODO: Request target language here. 
+            // For now from language will always be english
+        }
+        
+        public bool CanExecute(object parameter)
+        {
+            // TODO: Only if device is connected to internet
+            return true;
+        }
+
+        public async void Execute(object parameter)
+        {
+            TranslateTextService translateTextService = new TranslateTextService();
+            string translatedText = await translateTextService.Translate();
+            _picture.TranslatedText = translatedText;
+        }
+    }
+}
