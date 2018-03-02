7dd3be024ca88f7984bdee23bc36f675f622bfe3
KampusHub <verbelesteban2@gmail.com>
2018-03-01 22:39:49 -0500

    TranslateCommand now using array

--- ImageTextTranslatorApp/ImageTextTranslatorApp/ImageTextTranslatorApp/Models/Commands/TranslateTextCommand.cs
+++ ImageTextTranslatorApp/ImageTextTranslatorApp/ImageTextTranslatorApp/Models/Commands/TranslateTextCommand.cs
@@ -8,9 +8,9 @@ namespace ImageTextTranslatorApp.Models.Commands
     {
         public event EventHandler CanExecuteChanged;
 
-        private Tuple<string, string> _inputOutputText;
+        private string[] _inputOutputText;
 
-        public TranslateTextCommand(Tuple<string, string> inputOutputText)
+        public TranslateTextCommand(string[] inputOutputText)
         {
             // TODO: Request target language here. 
             // For now from language will always be english
@@ -25,13 +25,12 @@ namespace ImageTextTranslatorApp.Models.Commands
 
         public async void Execute(object parameter)
         {
-            string text = _inputOutputText.Item1;
+            string text = _inputOutputText[0];
+
             // launch translate text service
             TranslateTextService translateTextService = new TranslateTextService(text);
-            string translatedText = await translateTextService.Translate();
-
             // set text and translated text
-            _inputOutputText = new Tuple<string, string>(text, translatedText);
+            _inputOutputText[1] = await translateTextService.Translate();
         }
     }
 }
