81797b42b7fbab5948376c1a8f598c58df8bcf83
KampusHub <verbelesteban2@gmail.com>
2018-03-10 16:52:23 -0500

    Added TargetLanguage Store abstraction to manage target translate language

---
+++ ImageTextTranslatorApp/ImageTextTranslatorApp/ImageTextTranslatorApp/Services/LanguageStore.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ImageTextTranslatorApp.Services
+{
+    public enum Language
+    {
+        Spanish,
+        French,
+        CantoneseTraditional,
+        ChineseSimplified,
+        ChineseTraditional,
+        German,
+        Italian,
+        Korean,
+        Russian
+    };
+
+    public class LanguageStore
+    {
+        private static readonly Lazy<LanguageStore> _instance = new Lazy<LanguageStore>(() => new LanguageStore());
+
+        public static LanguageStore Instance => _instance.Value;
+        
+        public static Language SelectedTargetLanguage { get; private set; } = Language.Spanish;
+        
+        public void SetTargetLanguage(Language language)
+        {
+            SelectedTargetLanguage = language;
+        }
+    }
+}
